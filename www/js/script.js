f5f131f308ccc6f46fa35ed6e21cabff7e8c8f8c
Chittipobng014 <tpl_army@hotmail.com>
2018-04-07 05:05:31 +0700

    Smar Care Page initial

--- www/js/script.js
+++ www/js/script.js
@@ -94,3 +94,19 @@ document.addEventListener('init', function (event) {
 
 
 });
+
+var prev = function() {
+    var carousel = document.getElementById('carousel');
+    carousel.prev();
+  };
+  
+  var next = function() {
+    var carousel = document.getElementById('carousel');
+    carousel.next();
+  };
+  
+  ons.ready(function() {
+    var carousel = document.addEventListener('postchange', function(event) {
+      console.log('Changed to ' + event.activeIndex)
+    });
+  });
