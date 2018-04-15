339f1bbc1da59aec71ea86ca732fe55a837e7737
Anantaya038 <kikz.anantaya@gmail.com>
2018-04-15 18:28:27 +0700

    edit data

--- www/js/chart.js
+++ www/js/chart.js
@@ -1,14 +1,13 @@
 ons.ready(function () {
-
     var ctx = document.getElementById("myChart").getContext('2d');
     console.log(ctx);
     var myChart = new Chart(ctx, {
         type: 'radar',
-        data: {
-            labels: ["Heart fit", "Motion fit", "Metabolism fit"],
+        data: {            //Top          //Right       //Left
+            labels: ["Metabolism fit", "Heart fit", "Motion fit"],
             datasets: [{
-                
-                data: [2.9, 2.9, 3.9],
+                      //top,right,left  
+                data: [2, 5, 3],
                 backgroundColor: [
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
@@ -23,6 +22,18 @@ ons.ready(function () {
             }]
         },
         options: {
+            scale: {
+                ticks: {
+                    beginAtZero: true,
+                    min: 0,
+                    max: 5,
+                    stepSize: 1
+                },
+                pointLabels: {
+                    fontColor: "black",
+                    fontSize: 15
+                }
+            },
            
             legend: {
                 display: false,
@@ -36,5 +47,5 @@ ons.ready(function () {
             }
         }
     });
-   
 });
+
