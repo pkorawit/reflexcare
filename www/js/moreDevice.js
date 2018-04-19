9b953ba5c3a3eb14d78d931510c41c52ba02ef27
Korawit Prutsachainimmit <korawit@Korawits-MacBook-Pro.local>
2018-04-20 05:33:18 +0700

    Add devices

--- www/js/moreDevice.js
+++ www/js/moreDevice.js
@@ -2,10 +2,22 @@
 ons.ready(function () {
     document.addEventListener('init', function (event) {
         var page = event.target;
-        var currentUser = page.data.currentUser;       
+        var currentUser = page.data.currentUser;
         if (page.id == "moreDevice") {
-            $('#charge2').click(function () {                
-                console.log('Charge2');
+            
+            //Control add icons
+            for(var i=0;i<currentUser.devices.length;i++){
+                var device = "#" + currentUser.devices[i];
+                $(device).attr("src","img/yes.png");
+            }
+
+            function addFitbit(device){
+
+                if(currentUser.devices.indexOf(device) != -1){
+                    return;
+                }
+
+                console.log(device);
                 var endUrl = "https://smartreflex-2018.firebaseapp.com/fitbit";
                 var url = "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CVM2&redirect_uri=https%3A%2F%2Fsmartreflex-2018.firebaseapp.com%2Ffitbit&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight";
                 var browser = cordova.InAppBrowser.open(url, '_blank', 'location=yes');
@@ -22,23 +34,35 @@ ons.ready(function () {
 
                         //Create mock up score data
                         SmartReflex.getScore(currentUser.profile.userid).then(function (message, score) {
-                            console.log(currentUser.profile.userid);  
-                            console.log(score);  
-                            if(score == null){
+                            console.log(currentUser.profile.userid);
+                            console.log(score);
+                            currentUser.devices.push(device);
+                            if (score == null) {
                                 //Get mock score template
                                 var mockUserID = "mock@smartreflex.info";
-                                SmartReflex.getScore(mockUserID).then(function (message, mockscore) {
-                                    //ons.notification.alert(message);   
+                                SmartReflex.getScore(mockUserID).then(function (message, mockscore) {  
                                     mockscore.userid = currentUser.profile.userid;
-                                    SmartReflex.addScore(mockscore).then(function (message, newscore) {
-                                        //ons.notification.alert(message);   
-                                        changeTab('views/smartReflex.html', 'SMART REFLEX', 1);                                        
+                                    SmartReflex.addScore(mockscore).then(function (message, newscore) {                                       
+                                        SmartReflex.updateUser(currentUser).then(function (message, newuser) {
+                                            changeTab('views/smartReflex.html', 'SMART REFLEX', 1);
+                                        });                                        
                                     });
                                 });
-                            }                          
+                            }
+                            else{
+                                changeTab('views/smartReflex.html', 'SMART REFLEX', 1);
+                            }
                         });
                     }
                 });
+            }
+
+            $('#charge2').click(function () {
+                addFitbit('charge2')
+            });
+
+            $('#aria2').click(function () {
+                addFitbit('aria2')
             });
 
         }
